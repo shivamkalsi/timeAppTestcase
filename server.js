ed33017488abe5eaa3af6bd5dc85990dfdee1e8c
shivam <shivamkalsi@gmail.com>
2017-12-20 04:36:53 +0530

    changes done for heroku

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var express=require('express');
 var app=express();
 
+const PORT=process.env.PORT||3000;
+
+app.use( function(req,res,next){
+	if(req.header('x-forwarded-proto')=='http')
+	{
+		next();
+	}
+	else{
+		res.redirect('http://'+req.hostname+req.url);
+	}
+});
+
 app.use(express.static('public'));
 
 
 
-var server=app.listen(3000,function()
+var server=app.listen(PORT,function()
 {
-	console.log('Server Started');
+	console.log('Server Started on port ' +PORT);
 });
